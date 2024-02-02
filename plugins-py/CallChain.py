aa68c0868a868ab754a891d603063c9147b4722c
smile-e3 <2278215957@qq.com>
2024-02-02 11:13:34 +0800

    fix:Headless模式ImportError

--- plugins-py/CallChain.py
+++ plugins-py/CallChain.py
@@ -1,23 +1,14 @@
-
 # Display call chain graph between two functions and output to the console.
-#@author fuzzywalls
-#@category TNS
-#@menupath TNS.Call Chain
+#@author smile
+#@category VulAnalysize
+#@menupath VulAnalysize.Call Chain
 
-
-
-import os
-import sys
-import tempfile
-from utils import graphviz
 
 from ghidra.util.graph import DirectedGraph, Vertex, Edge
 from ghidra.graph.viewer import GraphComponent
-# from ghidra.program.model.address import AddressFactory
 from ghidra.program.model.address import Address
 
 found_call_chain = False
-
 
 def get_references(caller, callee):
     """
@@ -44,16 +35,7 @@ def get_references(caller, callee):
     return ref_list
 
 
-def sanitize_dot(func):
-    """
-    Return a sanitized function name string compatible with DOT representation.
-
-    :param func: Function object
-    """
-    return str(func).replace("::", "\\")
-
-
-def print_call_chain(call_chain, dot):
+def print_call_chain(call_chain):
     """
     Print successful call chains to the console and add to the graph.
 
@@ -65,31 +47,13 @@ def print_call_chain(call_chain, dot):
     function_chain = []
 
     for function in call_chain:
-        references = []
-
-        function_name = sanitize_dot(function)
-
-        if function == call_chain[0]:
-            dot.node(function_name, str(function), style='filled',
-                     color='blue', fontcolor='white')
-        elif function == call_chain[-1]:
-            dot.node(function_name, str(function), style='filled',
-                     color='red', fontcolor='white')
-        else:
-            dot.node(function_name, str(function))
         if previous_function:
-            previous_function_name = sanitize_dot(previous_function)
-            dot.edge(previous_function_name, function_name)
             function_references[str(previous_function)] = get_references(
                 previous_function, function)
 
         previous_function = function
         function_chain.append(str(function))
-    # print("====================")
-    # print(function_chain)
-    # print("====================")
-    # print(function_references)
-    # print("********************")
+
 
     for function in function_chain:
         print function,
@@ -99,7 +63,7 @@ def print_call_chain(call_chain, dot):
     print ''
 
 
-def call_chain_recurse(call_chain, complete_call, dot):
+def call_chain_recurse(call_chain, complete_call):
     """
     Recurse from call_chain to complete_call, if found.
 
@@ -112,17 +76,15 @@ def call_chain_recurse(call_chain, complete_call, dot):
 
     function_list = call_chain[0].getCallingFunctions(monitor)
 
-    # print(function_list)
-
     for func in function_list:
         if func == complete_call:
-            print_call_chain([func] + call_chain, dot)
+            print_call_chain([func] + call_chain)
             found_call_chain = True
             continue
 
         if func in call_chain:
             continue
-        call_chain_recurse([func] + call_chain, complete_call, dot)
+        call_chain_recurse([func] + call_chain, complete_call)
 
 
 def discover_call_chain(from_function, to_function):
@@ -132,8 +94,7 @@ def discover_call_chain(from_function, to_function):
     :param from_function: Function start looking for path to next function.
     :param to_function: Function that, when/if found, indicates a chain.
     """
-    dot = graphviz.Digraph('Function Paths', format='png', strict=True)
-    call_chain_recurse([to_function], from_function, dot)
+    call_chain_recurse([to_function], from_function)
 
 
 func_man = currentProgram.getFunctionManager()
@@ -141,31 +102,18 @@ func_man = currentProgram.getFunctionManager()
 function_list = [function for function in func_man.getFunctions(True)]
 function_list.sort(key=lambda func: str(func))
 
-# print(function_list)
 
 for func in function_list:
-    # print("Function Name is: {}, function type is :{}, function's address is: {}".format(func, type(func), func.getBody()))
-    if func.getName() == 'main':
+    if func.getName() == 'main' or func.getName() == 'entry':
         print("Function Name is: {}, function type is :{}, function's address is: {}".format(func, type(func), func.getBody()))
         from_function = func
 
-# from_function = askChoice('Select function',
-#                           'Select the starting function',
-#                           function_list,
-#                           function_list[0])
 
 
 function_list.remove(from_function)
-# to_function = askChoice('Select function',
-#                         'Select the ending function',
-#                         function_list,
-#                         function_list[0])
+
 to_function = func_man.getFunctionAt(currentProgram.getAddressFactory().getAddress("0x000093fc"))
-# print(func_man.getFunctionAt(currentProgram.getAddressFactory().getAddress("0x000093fc")))
-print(type(to_function))
-print(to_function)
-# from_function = function("GetFileValue")
-# to_function = function("<EXTERNAL>::execve")
+
 print 'Finding x-refs from %s to %s\n' % (from_function, to_function)
 
 discover_call_chain(from_function, to_function)
